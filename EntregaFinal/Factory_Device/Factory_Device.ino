20d023bd0a825e9a059d24478b9d5e9fe284f6b5
marcbonastre <marc.bonastre35@gmail.com>
2021-06-07 23:40:49 +0200

    Entrega Final

--- EntregaFinal/Factory_Device/Factory_Device.ino
+++ EntregaFinal/Factory_Device/Factory_Device.ino
@@ -24,17 +24,13 @@ Adafruit_Si7021 SI702x = Adafruit_Si7021();
 
 #define TEMP_AMB2 "Amb2/1/Temp"
 #define HUM_AMB2 "Amb2/1/Hum"
-#define LUZ_AMB2 "Amb2/1/Lux"
 
 #define COMMAND_TOPIC "cmd"
 #define TEST_TOPIC "test"
 
 //DEFINE PINS
-#define DHT_PIN 23     // Defines pin number to which the sensor is connected
+#define DHT_PIN 17     // Defines pin number to which the sensor is connected
 #define DHT_TYPE DHT11 // Defines the sensor type. It can be DHT11 or DHT22
-
-#define DHT2_PIN 17     // Defines pin number to which the sensor is connected
-#define DHT2_TYPE DHT11 // Defines the sensor type. It can be DHT11 or DHT22
 
 #define ECHO_PIN 36 // Analog input that receives the echo signal
 #define TRIG_PIN 12 // Digital output that sends the trigger signal
@@ -54,7 +50,6 @@ int voltage_measure;
 int lux;
 
 DHT dhtSensor(DHT_PIN, DHT_TYPE); // Defines the sensor dht
-DHT dhtSensor2(DHT2_PIN, DHT2_TYPE); // Defines the sensor dht 2
 
 // Replace the next variables with your Wi-Fi SSID/Password
 const char *WIFI_SSID = "****";
@@ -70,11 +65,9 @@ const bool RETAINED = true;
 const int QoS = 0; // Quality of Service for the subscriptions
 
 float distance=0;
-float temperature=0.0;
-float humidity=0.0;
+float temperatureDHT=0.0;
+float humidityDHT=0.0;
 float light=0.0;
-float temperature_tk1=0.0;
-float humidity_tk1=0.0;
 
 //CJMCU Sensors
 float bmp280Temp = 0.0;
@@ -92,7 +85,6 @@ void setup() {
   Serial.begin(9600); // Starts the serial communication
   Serial.println("\nBooting device...");
   dhtSensor.begin(); // Starts sensor communication
-  dhtSensor2.begin(); // Starts sensor communication
 
   pinMode(ECHO_PIN, INPUT);  // Sets the ECHO_PIN as an Input
   pinMode(TRIG_PIN, OUTPUT); // Sets the TRIG_PIN as an Output
@@ -108,10 +100,7 @@ void setup() {
 
   TimerHandle_t xTimer = xTimerCreate("sendData", REFRESH_RATE, pdTRUE, (void *) 0, sendData);
   xTimerStart(xTimer, 0);
-
-//  TimerHandle_t xTimer2 = xTimerCreate("readSensor", REFRESH_RATE_READ, pdTRUE, (void *) 0, sendData);
-//  xTimerStart(xTimer2, 0);
-
+ 
 
   //CJMCU Sensor
   Wire.begin(21,22);                  // put here the Pins of I2C
@@ -165,14 +154,38 @@ void loop() {
 
   checkConnections(); // We check the connection every time
   getCJMCU_data();
-  temperature = SI702x.readTemperature(); // Reads the temperature, it takes
-  humidity = SI702x.readHumidity(); //dhtSensor.readHumidity();
-  temperature_tk1 = dhtSensor2.readTemperature(); // Reads the temperature, it takes
-  humidity_tk1 = dhtSensor2.readHumidity();
+  temperatureDHT = dhtSensor.readTemperature();
+  humidityDHT = dhtSensor.readHumidity();
   distance = getDistance();
-  light = luz();
+  light = getluz();
 
 }
+
+float getDistance() {
+  digitalWrite(TRIG_PIN, LOW); // Clear the TRIG_PIN by setting it LOW
+  delayMicroseconds(5);
+
+  // Trigger the sensor by setting the TRIG_PIN to HIGH for 10 microseconds
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH); // pulseIn() returns the duration (length of the pulse) in microseconds
+
+  //Serial.println("Distancia" + String(duration * 0.034 / 2));
+
+  return duration * 0.034 / 2; // Returns the distance in cm
+}
+
+float getluz(){
+ voltage_measure = analogRead(LDR_PIN);  // Reads the value from the pin in a 0-4095 resolution corresponding to a linear 0-3.3V
+
+ lux = voltage_measure * DARKNESS_RES * 10 / (BRIGHTNESS_RES * CALIBRARION_RES * (pow(2.0, ANALOG_BIT_RESOLUTION) - voltage_measure)); // Use with LDR & Vcc
+
+ //Serial.println("luz" + String(lux));
+
+ return lux;
+ }
 
 void getCJMCU_data() {
   
@@ -204,7 +217,6 @@ void sendData(TimerHandle_t xTimer){
   publishData(LUZ_AMB11, light);
 
   publishFactoryReception("Reception");
-  publishData("Recep1_Tnk1_T", temperature_tk1);
 }
 
 /* Additional functions */
@@ -298,24 +310,6 @@ void checkConnections() {
   }
 }
 
-void publishFactoryAmbient(char* Topic) {
-  static const String topicStr = createTopic(Topic);
-  static const char *topic = topicStr.c_str();
-
-  StaticJsonDocument<128> doc; // Create JSON document of 128 bytes
-  char buffer[128]; // Create the buffer where we will print the JSON document
-                    // to publish through MQTT
-
-  doc["t"] = temperature;
-  doc["h"] = humidity;
-  doc["l"] = light;
-  
-  // Serialize the JSON document to a buffer in order to publish it
-  serializeJson(doc, buffer);
-  mqttClient.publish(topic, buffer, RETAINED);
-  Serial.println(" <= " + String(topic) + ": " + String(buffer));
-}
-
 void publishData(char* Topic, float value) {
   //static const String topicStr = createTopic(Topic);
   //static const char *topic = topicStr.c_str();
@@ -324,6 +318,24 @@ void publishData(char* Topic, float value) {
   
   mqttClient.publish(topic, String(value).c_str(), RETAINED);
   Serial.println(" <= " + String(topic) + ": " + String(value));
+}
+
+void publishFactoryAmbient(char* Topic) {
+  static const String topicStr = createTopic(Topic);
+  static const char *topic = topicStr.c_str();
+
+  StaticJsonDocument<128> doc; // Create JSON document of 128 bytes
+  char buffer[128]; // Create the buffer where we will print the JSON document
+                    // to publish through MQTT
+
+  doc["t"] = temperatureDHT;
+  doc["h"] = humidityDHT;
+  doc["l"] = light;
+  
+  // Serialize the JSON document to a buffer in order to publish it
+  serializeJson(doc, buffer);
+  mqttClient.publish(topic, buffer, RETAINED);
+  Serial.println(" <= " + String(topic) + ": " + String(buffer));
 }
 
 void publishFactoryReception(char* Topic) {
@@ -335,47 +347,14 @@ void publishFactoryReception(char* Topic) {
                      // to publish through MQTT
 
   JsonObject R1 = doc.createNestedObject("R1"); // Add another Object
-  R1["t_tk1"] = temperature_tk1;
-  R1["l_tk1"] = humidity_tk1;
-  R1["t_tk2"] = temperature_tk1 + 0.34;
-  R1["l_tk2"] = humidity_tk1 + 5;
+  R1["t_tk1"] = temperatureDHT;
+  R1["l_tk1"] = distance * random(0.8, 1.8);
+  R1["t_tk2"] = temperatureDHT * random(0.5, 1.5);
+  R1["l_tk2"] = distance * random(0.8, 1.8);
   R1["d_cmn"] = distance;
-
-  JsonObject R2 = doc.createNestedObject("R2"); // Add another Object
-  R2["t_tk1"] = temperature_tk1;
-  R2["l_tk1"] = humidity_tk1;
-  R2["t_tk2"] = temperature_tk1 + 0.34;
-  R2["l_tk2"] = humidity_tk1 + 5;
-  R2["d_cmn"] = distance;
   
   // Serialize the JSON document to a buffer in order to publish it
   size_t n = serializeJson(doc, buffer);
   mqttClient.publish_P(topic, buffer, n); // No RETAINED option
   Serial.println(" <= " + String(topic) + ": " + String(buffer));
 }
-
-float getDistance() {
-  digitalWrite(TRIG_PIN, LOW); // Clear the TRIG_PIN by setting it LOW
-  delayMicroseconds(5);
-
-  // Trigger the sensor by setting the TRIG_PIN to HIGH for 10 microseconds
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-
-  long duration = pulseIn(ECHO_PIN, HIGH); // pulseIn() returns the duration (length of the pulse) in microseconds
-
-  //Serial.println("Distancia" + String(duration * 0.034 / 2));
-
-  return duration * 0.034 / 2; // Returns the distance in cm
-}
-
-float luz(){
- voltage_measure = analogRead(LDR_PIN);  // Reads the value from the pin in a 0-4095 resolution corresponding to a linear 0-3.3V
-
- lux = voltage_measure * DARKNESS_RES * 10 / (BRIGHTNESS_RES * CALIBRARION_RES * (pow(2.0, ANALOG_BIT_RESOLUTION) - voltage_measure)); // Use with LDR & Vcc
-
- //Serial.println("luz" + String(lux));
-
- return lux;
- }
